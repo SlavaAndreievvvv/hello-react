7e1d433ffeda43dcb4839f542982563e904d640a
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-02-21 13:23:24 +0200

    🐛 fix mistakes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,14 @@
 import styles from './App.module.css'
-import success from '../assets/success.svg'
-import warning from '../assets/warning.svg'
-import error from '../assets/error.svg'
-import { Notifications } from '../components/Notifications';
+import { Notification } from '../components/Notification';
 
 
 export const App = () => {
   return (
     <div className={styles.container}>
       <div className={styles.cardList}>
-        <Notifications variant="warning" message="Notification" icon={warning} className={styles.margin} color = "#FD9B00" />
-        <Notifications variant="success" message="Notification" icon={success} className={styles.margin} color = "#14B985"/>
-        <Notifications variant="error"   message="Notification"   icon={error}   className={styles.margin} color = "#FF3F54" />
+        <Notification variant="warning" className={styles.margin}>Notification</Notification>
+        <Notification variant="success" className={styles.margin}>Notification</Notification>
+        <Notification variant="error" className={styles.margin}>Notification</Notification>
       </div>
     </div>
   );
